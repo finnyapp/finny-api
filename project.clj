b07b76cbd7cad2f55ea9fb468e6f9c3669ad6054
Gregório Melo <gregorio@gregoriomelo.com>
2015-01-21 11:48:45 +1000

    Hello world as json

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.13"]]
--- src/finny_api/core/handler.clj
+++ src/finny_api/core/handler.clj
@@ -1,10 +1,15 @@
 (ns finny-api.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :as middleware]
+            [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn as-json-response [request]
+  (response {:body {:message "Hello world: "}}))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (middleware/wrap-json-response as-json-response))
   (route/not-found "Not Found"))
 
 (def app
