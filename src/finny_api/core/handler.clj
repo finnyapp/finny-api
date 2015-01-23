7915fb15ceefbb719717f968835f0ece2281f65f
Gregório Melo <gregorio@gregoriomelo.com>
2015-01-23 14:45:01 +1000

    Outputting received message

--- src/finny_api/core/handler.clj
+++ src/finny_api/core/handler.clj
@@ -1,16 +1,20 @@
 (ns finny-api.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.json :as middleware]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defn print-request-message [request]
+  (response (get-in request [:message])))
 
 (defn as-json-response [request]
   (response {:body {:message "Hello world: "}}))
 
 (defroutes app-routes
-  (GET "/" [] (middleware/wrap-json-response as-json-response))
+  (GET "/" [] as-json-response)
+  (POST "/" {body :body} (print-request-message body))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-json-response (wrap-json-body app-routes {:keywords? true :bigdecimals? true})))
