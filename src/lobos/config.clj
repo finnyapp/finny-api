27feee57c9dcfbf604f2feefefff2334693b8324
Gregório Melo <gregorio@gregoriomelo.com>
2015-02-06 15:47:40 +1000

    Complying with Heroku

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,12 +1,29 @@
 (ns lobos.config
-  (:use lobos.connectivity))
+  (:refer-clojure :exclude [replace reverse])
+  (:use [clojure.string :as str]
+        lobos.connectivity)
+  (:import (java.net URI)))
 
-(defn db-url []
-  (or (System/getenv "DATABASE_URL") "//localhost:5432/finny"))
+(defn heroku-db
+  "Generate the db map according to Heroku environment when available."
+  []
+  (when (System/getenv "DATABASE_URL")
+    (let [url (URI. (System/getenv "DATABASE_URL"))
+          host (.getHost url)
+          port (if (pos? (.getPort url)) (.getPort url) 5432)
+          path (.getPath url)]
+      (merge
+        {:subname (str "//" host ":" port path)}
+        (when-let [user-info (.getUserInfo url)]
+          {:user (first (str/split user-info #":"))
+           :password (second (str/split user-info #":"))
+           :ssl "true"
+           :sslfactory "org.postgresql.ssl.NonValidatingFactory"})))))
 
 (def db
-  {:classname "org.postgresql.Driver"
-   :subprotocol "postgresql"
-   :subname (db-url)})
+  (merge {:classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :subname "//localhost:5432/finny"}
+         (heroku-db)))
 
 (open-global db)
