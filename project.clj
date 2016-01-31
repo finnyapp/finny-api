066e9a351eaf8c3ea172d38094f6d06a65099b73
Gregório Melo <gregorio@gregoriomelo.com>
2016-01-31 20:18:03 -0200

    Updating libs

--- project.clj
+++ project.clj
@@ -2,24 +2,23 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [cheshire "5.4.0"]
-                 [postgresql "9.1-901.jdbc4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [cheshire "5.5.0"]
+                 [postgresql "9.3-1102.jdbc41"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.7"]
+                 [org.slf4j/slf4j-log4j12 "1.7.14"]
                  [korma "0.4.0"]
                  [lobos "1.0.0-beta3"]
-                 [postgresql "9.1-901.jdbc4"]
                  [log4j/log4j "1.2.17" :exclusions  [javax.mail/mail
                                                      javax.jms/jms
                                                      com.sun.jmdk/jmxtools
                                                      com.sun.jmx/jmxri]]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler finny-api.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
