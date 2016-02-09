5c5b919865817e4a4740b402233b270665234726
Gregório Melo <gregorio@gregoriomelo.com>
2016-02-08 22:21:27 -0200

    A bit more coverage for routing

--- test/finny_api/core/handler_test.clj
+++ test/finny_api/core/handler_test.clj
@@ -5,33 +5,42 @@
             [finny-api.core.handler :refer :all]
             [cheshire.core :refer :all]))
 
-(defn get-transactions-total [] (app (mock/request :get "/transactions/total")))
-
-(defn options [] (app (mock/request :options "/")))
-
-(defn get-invalid-route [] (app (mock/request :get "/invalid-route")))
-
 (defn get-root [] (app (mock/request :get "/")))
-
-(defn post-to-root [] (app (mock/request :post "/")))
-
-(fact "OPTIONS is available"
-      (:status (options)) => 200
-      (.contains (:body (options)) "version") => true)
-
-(fact "An invalid route 404s"
-      (:status (get-invalid-route)) => 404
-      (.contains (:body (get-invalid-route)) "Not Found") => true)
-
 (fact "Root is a `Hello, world!` message"
       (:status (get-root)) => 200
       (get-in (parse-string (:body (get-root)) true) [:message]) => "Hello, world!")
 
-(fact "POSTing to root gives you nothing"
-      (:status (post-to-root)) => 405)
-
+(defn get-transactions-total [] (app (mock/request :get "/transactions/total")))
 (fact "Gets the total value of transactions"
       (:status (get-transactions-total)) => 200
       (:total (parse-string (:body (get-transactions-total)) true)) => 5
       (provided
         (transactions/get-transactions-total) => 5))
+
+(defn get-transactions [] (app (mock/request :get "/transactions")))
+(fact "Gets all transactions"
+      (:status (get-transactions)) => 200
+      (:transactions (parse-string (:body (get-transactions)) true)) => [{:value 1} {:value 2}]
+      (provided
+        (transactions/get-transactions) => [{:value 1} {:value 2}]))
+
+(defn get-transaction [] (app (mock/request :get "/transaction/7")))
+(fact "Gets transaction"
+      (:status (get-transaction)) => 200
+      (:value (parse-string (:body (get-transaction)) true)) => 14
+      (provided
+        (transactions/get-transaction "7") => {:value 14}))
+
+(defn options [] (app (mock/request :options "/")))
+(fact "OPTIONS is available"
+      (:status (options)) => 200
+      (.contains (:body (options)) "version") => true)
+
+(defn get-invalid-route [] (app (mock/request :get "/invalid-route")))
+(fact "An invalid route 404s"
+      (:status (get-invalid-route)) => 404
+      (.contains (:body (get-invalid-route)) "Not Found") => true)
+
+(defn post-to-root [] (app (mock/request :post "/")))
+(fact "POSTing to root gives you nothing"
+      (:status (post-to-root)) => 405)
