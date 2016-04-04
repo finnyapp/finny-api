14b09b379358e7cac80753aeecdf484797cc0768
Gregório Melo <gregorio@gregoriomelo.com>
2016-04-04 18:12:24 -0300

    An acceptance test that gets all transactions! Yay!

--- test/acceptance-tests/finny_api/transactions_acceptance_test.clj
+++ test/acceptance-tests/finny_api/transactions_acceptance_test.clj
@@ -3,7 +3,13 @@
             [midje.sweet :refer :all]
             [ring.adapter.jetty :refer [run-jetty]]
             [clj-http.client :as client]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [finny-api.db.config :refer [db-spec]]
+            [clojure.java.jdbc :as j]
+            [honeysql.core :as sql]
+            [honeysql.helpers :as db-helpers]
+            [finny-api.db.transactions :as db]
+            [finny-api.transactions-fixtures :refer [small-expense heavy-expense]]))
 
 (def server (atom nil))
 
@@ -14,10 +20,36 @@
 (defn stop-server []
   (.stop @server))
 
+(defn- run-query [query]
+  (j/query db-spec (sql/format query)))
+
+(defn- insert-transaction [transaction]
+  (try
+    (db/create-transaction transaction)
+    (catch Exception e :ok)))
+
+(defn- clear-db []
+  (try
+    (run-query (-> (db-helpers/delete-from :transactions)))
+    (catch Exception e :ok)))
+
+(defn- prepare-db []
+  (clear-db)
+  (insert-transaction small-expense)
+  (insert-transaction heavy-expense))
+
 (against-background [(before :contents (start-server))
+                     (before :contents (prepare-db))
+                     (after  :contents (clear-db))
                      (after  :contents (stop-server))]
 
   (fact "Gets root" :at
         (let [response (client/get "http://localhost:3000")]
           (:status response) => 200
-          (:message (json/parse-string (:body response) true)) => "Hello, world!")))
+          (:message (json/parse-string (:body response) true)) => "Hello, world!"))
+
+  (fact "Gets all transactions" :at
+        (let [response (client/get "http://localhost:3000/transactions")]
+          (:status response) => 200
+          (map #(select-keys % [:value :comments]) (:transactions (json/parse-string (:body response) true)))
+            => (vector small-expense heavy-expense))))
---
+++ test/acceptance-tests/finny_api/transactions_fixtures.clj
@@ -0,0 +1,7 @@
+(ns finny-api.transactions-fixtures)
+
+(def small-expense
+  {:value 5.0 :comments "A small expense"})
+
+(def heavy-expense
+  {:value 999.0 :comments "Gosh!"})
